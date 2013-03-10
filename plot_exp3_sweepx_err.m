7247d10c43498d65d221d4f694f425b9807b36cf
dankearney <daniel.kearney@students.olin.edu>
2013-03-10 15:17:06 -0400

    making sure its all pushed

---
+++ plot_exp3_sweepx_err.m
@@ -0,0 +1,31 @@
+clf
+clear all
+load exp3_sweepx_400kOHMS
+loglog(I_x_src, I_z_meas,'bo')
+hold on
+I_z_exp = I_x_src .^ 2 ./ .5e-6;
+loglog(I_x_src, I_z_exp,'b-','linewidth',2)
+
+clear all
+load exp3_sweepx_40kOHMS
+loglog(I_x_src, I_z_meas,'ro')
+hold on
+I_z_exp = I_x_src .^ 2 ./ .5e-5;
+loglog(I_x_src, I_z_exp,'r-','linewidth',2)
+
+clear all
+load exp3_sweepx_4kOHMS
+loglog(I_x_src, I_z_meas,'go')
+hold on
+I_z_exp = I_x_src .^ 2 ./ .5e-4;
+loglog(I_x_src, I_z_exp, 'g-','linewidth',2)
+
+
+loglog(I_x_src, I_x_src*10*3,'k','linewidth',2)
+
+xlim([10^-7 max(I_x_src)])
+legend('500 nA experimental','500 nA theoretical','5 uA experimental','5 uA theoretical','50 uA experimental','50 uA theoretical','linear error fit','location','northwest')
+xlabel 'I_x (Amps)'
+ylabel 'I_z (Amps)'
+title('Translinear Circuit 2, Sweeping I_x', 'fontsize',20)
+saveas(gcf, 'exp3_sweepx_err.png')
